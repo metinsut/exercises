61ed44cbe6ef9bb7f2d3ac03bd0a7cda56a8d02e
mtnst <metindir@gmail.com>
2017-08-24 00:29:00 +0300

    input

--- src/components/Input.vue
+++ src/components/Input.vue
@@ -2,9 +2,10 @@
   <div class="input">
     <v-header></v-header>
     <div class="wrapper">
-      <div class="style1">
-        <input type="text" name="" value="">
-        <span></span>
+      <div class="mtn">
+        <input class="effect" type="text" placeholder="Placeholder Text">
+        <span class="focus-border"></span>
+        <span class="focus-border1"></span>
       </div>
     </div>
   </div>
@@ -27,29 +28,62 @@ export default {
 <style lang="scss" scoped>
 @import "../assets/common/common";
 
-.wrapper {
-  height: 300px;
-  .style1 {
-    input {
-      display: inline-block;
-      color: rgba(52, 73, 94,1.0);
-      height:50px;
-      width: 300px;
-      border: none;
-      &:focus {
-        outline: none;
-      }
-      &:focus:span {
-        transform: scalex(1);
-      }
-    }
-    span {
-      display: block;
-      content: '';
-      border-bottom: 3px solid rgba(192, 57, 43,1.0);
-      transform: scalex(0);
-      transition: transform 500ms ease-in-out;
-    }
-  }
+
+
+.mtn {
+  float: left;
+  width: 50%;
+  margin: 40px 3%;
+  position: relative;
+}
+
+input[type="text"] {
+  font: 15px/24px "Lato", Arial, sans-serif;
+  color: #333;
+  width: 100%;
+  box-sizing: border-box;
+  letter-spacing: 1px;
+}
+
+:focus {
+  outline: none;
+}
+
+.effect {
+  border: 0;
+  padding: 7px 0;
+  // border-bottom: 1px solid #ccc;
+}
+
+.effect~.focus-border {
+  position: absolute;
+  bottom: 0;
+  left: 50%;
+  width: 0;
+  height: 2px;
+  background-color: #3399FF;
+  transition: 1s;
+}
+
+.effect~.focus-border1 {
+  position: absolute;
+  bottom: 0;
+  left: 50%;
+  width: 0;
+  height: 2px;
+  background-color: red;
+  transition: 1s;
+}
+
+.effect:focus~.focus-border {
+  width: 50%;
+  transition: 1s;
+  left: 50%;
+}
+
+.effect:focus~.focus-border1 {
+  width: 50%;
+  transition: 1s;
+  left: 0;
 }
 </style>
