aa0855aef0761a8d08fe074227a9d4f65ed4f16f
mtnst <metindir@gmail.com>
2017-09-05 10:26:13 +0300

    edited

--- src/App.vue
+++ src/App.vue
@@ -11,32 +11,32 @@ export default {
 </script>
 
 <style lang="scss">
-
 @font-face {
-	font-family: 'Roboto';
-	src: url('./assets/fonts/Roboto-Regular.eot');
-	src: url('./assets/fonts/Roboto-Regular.woff') format('woff'),
-		url('./assets/fonts/Roboto-Regular.ttf') format('truetype');
-	font-weight: normal;
-	font-style: normal;
+  font-family: 'Roboto';
+  src: url('./assets/fonts/Roboto-Regular.eot');
+  src: url('./assets/fonts/Roboto-Regular.woff') format('woff'),
+  url('./assets/fonts/Roboto-Regular.ttf') format('truetype');
+  font-weight: normal;
+  font-style: normal;
 }
 
 
-*{
+* {
   margin: 0;
   padding: 0;
   font-family: Roboto;
   font-weight: 400;
 }
 
-body,html{
+body,
+html {
   background-color: white;
 }
 
-hr{
+hr {
   width: 100%;
   height: 5px;
-  color: rgba(192, 57, 43,1.0);
-  background-color: rgba(192, 57, 43,1.0);
+  color: rgba(192, 57, 43, 1.0);
+  background-color: rgba(192, 57, 43, 1.0);
 }
 </style>
